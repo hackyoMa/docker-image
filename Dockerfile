4651ec4346ca9fa481d4837c6d46928f98fa67a4
hackyo <137120918@qq.com>
2023-10-13 15:21:30 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # syntax=docker/dockerfile:latest
-FROM --platform=$TARGETPLATFORM hackyo/jre:17 AS build
-LABEL maintainer="137120918@qq.com" version="20230217"
+FROM --platform=$TARGETPLATFORM hackyo/jre:21 AS build
+LABEL maintainer="137120918@qq.com" version="20231013"
 ENV SPRING_BOOT_ADMIN_HOME=/usr/share/spring-boot-admin
 ENV JAVA_APP_JAR=${SPRING_BOOT_ADMIN_HOME}/spring-boot-admin.jar
-COPY build/libs/spring-boot-admin-3.0.0.jar ${SPRING_BOOT_ADMIN_HOME}/spring-boot-admin.jar
+COPY target/spring-boot-admin-3.1.5.jar ${SPRING_BOOT_ADMIN_HOME}/spring-boot-admin.jar
 HEALTHCHECK --interval=10s --timeout=5s --start-period=30s --retries=3 CMD curl -f http://localhost:8080/actuator/health || exit 1
 EXPOSE 8080
 ENTRYPOINT java -jar ${JAVA_APP_JAR} ${JAVA_OPTIONS}
--- src/main/java/com/github/hackyoma/springbootadmin/config/SecuritySecureConfig.java
+++ src/main/java/com/github/hackyoma/springbootadmin/config/SecuritySecureConfig.java
@@ -1,6 +1,7 @@
 package com.github.hackyoma.springbootadmin.config;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.UUID;
 
@@ -35,33 +40,45 @@ public class SecuritySecureConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setTargetUrlParameter("redirectTo");
         successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
 
-        return http.authorizeHttpRequests()
-                .requestMatchers(this.adminServer.path("/assets/**")).permitAll()
-                .requestMatchers(this.adminServer.path("/actuator/info")).permitAll()
-                .requestMatchers(this.adminServer.path("/actuator/health")).permitAll()
-                .requestMatchers(this.adminServer.path("/login")).permitAll()
-                .anyRequest().authenticated().and()
-                .formLogin().loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
-                .logout().logoutUrl(this.adminServer.path("/logout")).and()
+        return http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher(this.adminServer.path("/assets/**")))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher(this.adminServer.path("/actuator/info")))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher(this.adminServer.path("/actuator/health")))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher(this.adminServer.path("/login")))
+                        .permitAll()
+                        .dispatcherTypeMatchers(DispatcherType.ASYNC)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .formLogin((formLogin) -> formLogin.loginPage(this.adminServer.path("/login")).successHandler(successHandler))
+                .logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout")))
                 .httpBasic(Customizer.withDefaults())
-                .csrf().disable()
-                .rememberMe().key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600).and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600))
                 .build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
         return new InMemoryUserDetailsManager(
                 User.withUsername(security.getUser().getName())
-                        .password("{noop}" + security.getUser().getPassword())
+                        .password(passwordEncoder.encode(security.getUser().getPassword()))
                         .roles("USER")
                         .build()
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
